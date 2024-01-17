f3e39edfe43c4356c04726176d92dc526ee1aa10
Calvin Lee <jinwoo0451@naver.com>
2024-01-17 21:59:52 +0900

    feat: set up auto-scoring

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { Solution } from "./solution";
-import mysql2 from "mysql2";
+// import mysql2 from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
@@ -15,27 +15,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-/**
- * DB 연결하는 테스트 부분입니다.
- */
-// 최대한 이 부분은 수정하지 마시고 root로 접속해서 사용해주세요.
-const connection = mysql2.createConnection({
-  host: "localhost",
-  user: process.env.MYSQL_ROOT_USER, // 그럼에도 불구하고 다른 USER로 하시고 싶은 경우 .env파일 변경 후 process.env.MYSQL_USER로 수정해주시면 됩니다.
-  password: process.env.MYSQL_ROOT_PASSWORD, // 다른 USER로 하시고 싶은 경우 .env파일 변경 후 process.env.MYSQL_PASSWORD로 수정해주시면 됩니다.
-  database: process.env.MYSQL_DATABASE,
-  port: 3306,
-});
-
-connection.connect();
-
-connection.query("SELECT * from employees", (error, rows) => {
-  if (error) throw error;
-  console.log("User info is: ", rows);
-});
-
-// connection.end();
-// DB 연결 테스트 끝
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello Chainapsis!");
@@ -45,7 +24,7 @@ app.get("/", (req: express.Request, res: express.Response) => {
 
 /**
  * 1번 문제입니다.
- * /src/solution/solution.ts에서 solution_1_a 함수를 구현해주세요.
+ * /src/solution/solution.ts에서 solution_1_1, solution_1_2 함수를 구현해주세요.
  * Notion에 있는 문제 설명을 참고해주세요.
  */
 app.get(
@@ -72,8 +51,8 @@ app.post(
  * Notion에 있는 문제 설명을 참고해주세요.
  */
 app.get("/solution/2", async (req: express.Request, res: express.Response) => {
-  const solution = await Solution.solution_2();
-  res.send(solution);
+    const solution = Solution.solution_2();
+    res.send(solution);
 });
 
 //------------------------------------------------------
@@ -85,7 +64,7 @@ app.get("/solution/2", async (req: express.Request, res: express.Response) => {
  * 새로운 파일이 추가될 예정이고 해당 파일의 클래스를 구현해주시면 됩니다.
  */
 app.get("/solution/3", async (req: express.Request, res: express.Response) => {
-  const solution = await Solution.solution_3();
+  const solution = Solution.solution_3();
   res.send(solution);
 });
 
--- src/solution.test.ts
+++
@@ -1,7 +0,0 @@
-import { Hash } from "@keplr-wallet/crypto"
-
-const toSHA256 = (str : string) : string => {
-    const hash = Hash.keccak256(new TextEncoder().encode(str));
-    return Buffer.from(hash).toString("hex"); ;
-}
-
